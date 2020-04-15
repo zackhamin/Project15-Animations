d2573e7e8213da3c14b4363f368ae8a7b3e009b9
Zack <ishaqamincoop@icloud.com>
2020-04-15 10:30:34 +0100

    first commit

---
+++ Project15-Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project15-Animation
+//
+//  Created by Ishaq Amin on 15/04/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
